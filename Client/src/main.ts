1a8340a3526faf94a6535361a706d46cb3e756f7
Jardine <jardine.miller@gmail.com>
2022-12-05 00:05:40 +0000

    - Delete unused starter component - Included Prime Vue, Flex and Icons - Messing around with fonts - Changed up the unit tests & cypress tests

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,11 +1,12 @@
+import PrimeVue from "primevue/config";
 import { createApp } from "vue";
 import router from "./router";
 import App from "./App.vue";
-
 import "./assets/main.css";
 
 const app = createApp(App);
 
 app.use(router);
+app.use(PrimeVue);
 
 app.mount("#app");
