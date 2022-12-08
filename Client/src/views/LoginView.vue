e0680cab087e4cfdcd8c46efa5540122136d686b
Jardine <jardine.miller@gmail.com>
2022-12-08 15:42:26 +0000

    - LoginView password regex tweaks - Added validation.ts file

--- Client/src/views/LoginView.vue
+++ Client/src/views/LoginView.vue
@@ -1,12 +1,131 @@
-<script lang="ts"></script>
+<script lang="ts">
+    import Validation from "@/validation/validation";
+    export default {
+        data() {
+            return {
+                username: "",
+                password: "",
+                submitted: false,
+                passwordRegex: Validation.Auth.Password.Pattern,
+                impossibleRegex:
+                    "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])(?=.{8,})[A-Za-z\\d@$!%*?&]+$",
+            };
+        },
+        methods: {
+            handleSubmit() {
+                this.submitted = true;
+            },
+        },
+    };
+</script>
 
 <template>
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <h5>Heading 5</h5>
-    <h6>Heading 6</h6>
+    <div class="form-demo">
+        <div class="flex justify-content-center align-content-center">
+            <div class="card">
+                <h5 class="text-center">Login</h5>
+                <form
+                    @submit.prevent="handleSubmit()"
+                    class="p-fluid"
+                >
+                    <div class="field">
+                        <span class="p-float-label p-input-icon-left">
+                            <InputText
+                                id="name"
+                                v-model="username"
+                                :class="{
+                                    'p-invalid': submitted,
+                                }"
+                            />
+                            <i class="pi pi-id-card"></i>
+                            <label for="name">Username *</label>
+                        </span>
+                        <small
+                            v-if="submitted"
+                            class="p-error"
+                            >This is an error</small
+                        >
+                    </div>
+                    <div class="field">
+                        <span class="p-float-label p-input-icon-left">
+                            <Password
+                                id="password"
+                                v-model="password"
+                                :class="{
+                                    'p-invalid': submitted,
+                                }"
+                                toggleMask
+                                :medium-regex="impossibleRegex"
+                                :strong-regex="passwordRegex"
+                            >
+                                <template #header>
+                                    <h6>Pick a password</h6>
+                                </template>
+                                <template #footer="sp">
+                                    {{ sp.level }}
+                                    <Divider />
+                                    <p class="m-2">Requirements:</p>
+                                    <ul
+                                        class="pl-2 ml-2 mt-0"
+                                        style="line-height: 1.5"
+                                    >
+                                        <li>
+                                            At least one lowercase
+                                        </li>
+                                        <li>
+                                            At least one uppercase
+                                        </li>
+                                        <li>At least one numeric</li>
+                                        <li>
+                                            At least one special
+                                            character
+                                        </li>
+                                        <li>Minimum 6 characters</li>
+                                    </ul>
+                                </template>
+                            </Password>
+                            <i class="pi pi-lock"></i>
+                            <label
+                                for="password"
+                                :class="{
+                                    'p-error': submitted,
+                                }"
+                                >Password *</label
+                            >
+                        </span>
+                        <small
+                            v-if="submitted"
+                            class="p-error"
+                            >This is an error</small
+                        >
+                    </div>
+                    <Button
+                        type="submit"
+                        label="Submit"
+                        class="mt-2"
+                    />
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style></style>
+<style scoped>
+    .card {
+        min-width: 450px;
+    }
+
+    form {
+        margin-top: 2rem;
+    }
+
+    .field {
+        margin-bottom: 1.5rem;
+    }
+
+    @media screen and (max-width: 840px) {
+        .card {
+            width: 80%;
+        }
+    }
+</style>
