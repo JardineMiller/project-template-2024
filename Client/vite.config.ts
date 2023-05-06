51a619b5b43282c60d47d144ce86eb349deb6f11
Jardine <jardine.miller@gmail.com>
2023-05-06 17:44:13 +0100

    Fix for vitest config

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,14 +1,20 @@
+import type { UserConfig } from "vitest/config";
 import { fileURLToPath, URL } from "node:url";
 
 import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
     plugins: [vue()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
+    },
+    test: {
+        globals: true,
+        environment: "jsdom",
     },
 });
